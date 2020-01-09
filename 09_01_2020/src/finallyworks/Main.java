b9616f198bb7d3e7261ce352c11513ab0089ec3b
ad <ad@192.168.1.65>
2020-01-09 04:02:38 -0800

    today

---
+++ 09_01_2020/src/finallyworks/Main.java
@@ -0,0 +1,20 @@
+package finallyworks;
+public class Main {
+static int count=0;
+ public static void main (String[] args) {
+	 while(true) {
+		 try {
+			 if (count++ == 0)
+				 throw new MyException("exception occured");
+		 }
+		 catch(MyException e
+	 System.out.println(e);
+	 }
+	 finally {
+		 System.out.println("in finally clause");
+		 if (count==2)
+		 break;
+	 
+ }
+}
+}
