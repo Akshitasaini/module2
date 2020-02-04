ac6d5632445efb3aebe952e7ed8ac793fbfa9518
ad <ad@192.168.1.65>
2020-02-04 14:53:23 +0530

    json to pojo

---
+++ simplejsondemo/src/com/example/Student.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+public class Student {
+private int id;
+private String firstName;
+private String lastName;
+}
